7b46f1f3fd4e2e29d6ab881c09680953ca2d6a45
Thiago Cajaiba <thiago.cajaiba@gmail.com>
2023-01-16 20:57:00 -0300

    limpeza em comentários

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -2,15 +2,10 @@ const toggleSwitch = document.getElementById("theme-toggle");
 const h2 = document.getElementById("theme");
 const icon = document.getElementById("icon");
 
-// Adiciona um evento de clique ao toggle switch
 toggleSwitch.addEventListener("change", function () {
-  // Alterna a classe "dark-mode" no body
   document.body.classList.toggle("dark-mode");
-
-  // Adiciona ou remove a classe "dark-mode" ao h2
   h2.classList.toggle("dark-mode");
 
-  // Verifica se o tema é dark ou light
   if (document.body.classList.contains("dark-mode")) {
     h2.innerHTML = "Dark Theme";
     icon.classList.remove("fa-sun");
